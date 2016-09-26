38a0e3c8c797cd1ba680f1fda121c6779f238216
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-26 23:05:19 +0900

    fix mistake.

--- cloud_dfs/database/__init__.py
+++ cloud_dfs/database/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from .db_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+from cloud_dfs.database.db_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 
 engine = create_engine('mysql+pymysql://{0}:{1}@{2}/{3}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME), convert_unicode=True)
@@ -11,4 +11,4 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    Base.metadata.create_all(bind=engine
+    Base.metadata.create_all(bind=engine)
