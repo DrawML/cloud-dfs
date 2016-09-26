f8c0bdcd271481e3b62c03b0b12f739b9cc68ef3
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-26 23:06:49 +0900

    first implemention finish. but it must be refined.

--- cloud_dfs/__init__.py
+++ cloud_dfs/__init__.py
@@ -1,4 +1,10 @@
+import os
 from flask import Flask, g, render_template, request, jsonify, url_for
+import sqlalchemy
+from cloud_dfs.library import generate_random_token
+from cloud_dfs.database.models import Data
+from cloud_dfs.file import FileManager
+from cloud_dfs.database import db_session, init_db
 
 
 def _helpful_msg_json():
@@ -11,6 +17,12 @@ def _helpful_msg_json():
 
 def create_app():
     app = Flask(__name__)
+
+    init_db()
+
+    data_file_root_dir = os.path.dirname(os.sys.modules[__name__].__file__) + '/files'
+    print("Data File Root Directory :", data_file_root_dir)
+    FileManager(data_file_root_dir)
 
     @app.errorhandler(404)
     def not_found(error):
@@ -30,14 +42,57 @@ def create_app():
 
     @app.route('/data', methods=['POST'])
     def put_data():
-        raise NotImplementedError
+        json_data = request.get_json(force=True)
+        print(json_data)
 
-    @app.route('/data/<token>', methods=['GET'])
-    def get_data(token):
-        raise NotImplementedError
+        name = json_data['name']
+        data = json_data['data']
+        token = generate_random_token()
+        hex_token = token.hex()
+        path = FileManager().store(hex_token, data)
 
-    @app.route('/data/<token>', methods=['DELETE'])
-    def del_data(token):
-        raise NotImplementedError
+        data_obj = Data(name, token, path)
+        db_session.add(data_obj)
+        db_session.commit()
+
+        print(token)
+        return jsonify({
+            'token': token.hex()
+        }), 201
+
+
+    @app.route('/data/<hex_token>', methods=['GET'])
+    def get_data(hex_token):
+        token = bytes.fromhex(hex_token)
+        try:
+            data_obj = db_session.query(Data).filter(Data.token == token).one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            return '', 404
+
+        print(data_obj)
+
+        with open(data_obj.path, 'r') as f:
+            data = f.read()
+
+        return jsonify({
+            'name': data_obj.name,
+            'data': data
+        }), 200
+
+    @app.route('/data/<hex_token>', methods=['DELETE'])
+    def del_data(hex_token):
+        token = bytes.fromhex(hex_token)
+        try:
+            data_obj = db_session.query(Data).filter(Data.token == token).one()
+            db_session.query(Data).filter(Data.token == token).delete()
+            db_session.commit()
+        except sqlalchemy.orm.exc.NoResultFound:
+            return '', 404
+
+        print(data_obj)
+
+        FileManager().remove(data_obj.path)
+
+        return '', 204
 
     return app
