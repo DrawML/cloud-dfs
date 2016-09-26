936cc0a19e62d096b44d06dd64494631e26b32cb
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-26 21:57:32 +0900

    rename of functions.py And add AutoIncrementEnum and SingletonMeta to library.py.

--- cloud_dfs/functions.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def generate_random_token(bytes_size: int = 512 // 8):
-    return bytes(random.getrandbits(8) for _ in range(bytes_size))
---
+++ cloud_dfs/library.py
@@ -0,0 +1,22 @@
+import random
+from enum import Enum
+
+
+def generate_random_token(bytes_size: int = 512 // 8):
+    return bytes(random.getrandbits(8) for _ in range(bytes_size))
+
+
+class AutoIncrementEnum(Enum):
+    def __new__(cls):
+        value = len(cls.__members__) + 1
+        obj = object.__new__(cls)
+        obj._value_ = value
+        return obj
+
+
+class SingletonMeta(type):
+    __instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
