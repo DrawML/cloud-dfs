4bd29b3f547169b107329dd9a3db355364441c23
Taeguk Kwon <xornrbboy@gmail.com>
2016-10-09 02:23:23 +0900

    Fix a bug that occurs in __repr__ of models.

--- cloud_dfs/database/__init__.py
+++ cloud_dfs/database/__init__.py
@@ -6,7 +6,7 @@ from cloud_dfs.database.db_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 engine = create_engine('mysql+pymysql://{0}:{1}@{2}/{3}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME),
                        convert_unicode=True,  pool_recycle=1800)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
